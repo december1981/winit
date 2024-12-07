8fcd7eca3cb29b02a6d415456a9e9db14b1661ed
Stephen Brown <december1981@gmail.com>
2024-12-07 17:16:40 +0000

    support caplock/numlock key mods

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1699,6 +1699,10 @@ bitflags! {
         const ALT = 0b100 << 6;
         /// This is the "windows" key on PC and "command" key on Mac.
         const SUPER = 0b100 << 9;
+        /// The "Caps lock" key.
+        const CAPS_LOCK = 0b100 << 12;
+        /// The "Num lock" key.
+        const NUM_LOCK = 0b100 << 15;
     }
 }
 
@@ -1721,6 +1725,16 @@ impl ModifiersState {
     /// Returns `true` if the super key is pressed.
     pub fn super_key(&self) -> bool {
         self.intersects(Self::SUPER)
+    }
+
+    /// Returns `true` if caps lock is on.
+    pub fn caps_lock_on(&self) -> bool {
+        self.intersects(Self::CAPS_LOCK)
+    }
+
+    /// Returns `true` if num lock is on.
+    pub fn num_lock_on(&self) -> bool {
+        self.intersects(Self::NUM_LOCK)
     }
 }
 
